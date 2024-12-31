a9ce95c3a710c84ee170236b514ff5b633e704bb
aqyuki <aqyuki@aqyuki.dev>
2025-01-01 00:47:34 +0900

    ✨ Update top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Hero } from '@/feature/hero'
+import { Link } from '@/feature/link'
+import { Profile } from '@/feature/profile'
+import { Skill } from '@/feature/skill'
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Profile />
+      <Skill />
+      <Link />
     </>
   )
 }
